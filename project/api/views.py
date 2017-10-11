b35512ce76de5841ffefb25789f97bfbe8cada5a
Martin Barreto <martin@xmartlabs.com>
2017-10-11 18:27:05 -0300

    refactor

--- project/api/views.py
+++ project/api/views.py
@@ -7,18 +7,17 @@ from project import db
 from sqlalchemy import exc
 
 
-
 users_blueprint = Blueprint('users', __name__, template_folder='./templates')
 
-@users_blueprint.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        username = request.form['username']
-        email = request.form['email']
-        db.session.add(User(username=username, email=email))
-        db.session.commit()
-    users = User.query.order_by(User.created_at.desc()).all()
-    return render_template('index.html', users=users)
+# @users_blueprint.route('/', methods=['GET', 'POST'])
+# def index():
+#     if request.method == 'POST':
+#         username = request.form['username']
+#         email = request.form['email']
+#         db.session.add(User(username=username, email=email))
+#         db.session.commit()
+#     users = User.query.order_by(User.created_at.desc()).all()
+#     return render_template('index.html', users=users)
 
 @users_blueprint.route('/ping', methods=['GET'])
 def ping_pong():
@@ -92,7 +91,7 @@ def get_single_user(user_id):
 @users_blueprint.route('/users', methods=['GET'])
 def get_all_users():
     """Get all users"""
-    users = User.query.all()
+    users = User.query.order_by(User.created_at.desc()).all()
     users_list = []
     for user in users:
         user_object = {
