f31a33c824f46e30711285d36ab455dfad7f8f2c
cheluskis <marcelo@seamwaz.com>
2017-12-04 20:53:39 -0300

    verify cellphone number, pr fixes, add tasks

--- project/tasks/twilio_tasks.py
+++ project/tasks/twilio_tasks.py
@@ -5,4 +5,4 @@ from project import twilio_client, app
 
 @celery.task
 def send_async_sms(to, from_, body):
-    return twilio_client.messages.create(to=to, from_=app.config[‘TWILIO_FROM_NUMBER’], body=body) (edited)
+    return twilio_client.messages.create(to=to, from_=app.config['TWILIO_FROM_NUMBER'], body=body)
